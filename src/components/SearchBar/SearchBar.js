2eb36f14dbd15092c52b52cda10a7e65090bb16b
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2025-10-05 11:41:02 -0400

    SearchBar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import "./SearchBar.scss";
+
+const SearchBar = ({ searchTerm, setSearchTerm }) => (
+  <div className="search-bar">
+    <input
+      className="search-bar__input"
+      type="text"
+      placeholder="Search products..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+  </div>
+);
+
+export default SearchBar;
