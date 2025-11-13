4ccc33ffffdc2cd9fe57093631444e6ebd417e37
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2025-11-13 00:10:37 -0500

    f

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ import CheckoutPage from "./pages/CheckoutPage/CheckoutPage.js";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import SignUpForm from "./components/SignUp/SignUp.js";
-import UsersLandingPage from "./pages/UserLandingPage/UserLandingPg.js";
+import UsersLandingPage from "./pages/UserLandingPage/UserLAndingPg.js";
 import Profile from "./components/Profile/Profile.js";
 import ProfilePage from "./pages/ProfilePage/ProfilePage.js";
 import LoginForm from "./components/Login/Login.js";
