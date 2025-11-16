2d7a042d8c84b70b1c827ee2c3aee0f1b5fff2cf
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2025-11-16 18:28:53 -0500

    manifest

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,25 +1,23 @@
 import React from "react";
 import "./ProductCard.scss";
 const ProductCard = ({ item, addToCart, subtotal, isSubscribed, onRemove }) => {
-  console.log(item.id);
-
   return (
     <div className="product-card">
       <img className="product-card__img" src={item.imageUrl} alt={item.name} />
-      <h4 className="product-card__title">{item.name}</h4>
-      <p className="product-card__origin">{item.origin}</p>
+      <h4 className="product-card__title">{item?.name}</h4>
+      <h4 className="product-card__title">${item?.quantity}</h4>
+      <p className="product-card__origin">{item?.origin}</p>
 
-      {(subtotal <= 50 && (
+      {subtotal <= 50 ? (
         <button
           className="product-card__button"
           onClick={() => addToCart(item.id)}
         >
           Add to Cart
         </button>
-      )) ||
-        (subtotal >= 50 && (
-          <button onClick={() => onRemove(item.id)}>remove</button>
-        ))}
+      ) : (
+        <button onClick={() => onRemove(item.id)}>Remove</button>
+      )}
     </div>
   );
 };
