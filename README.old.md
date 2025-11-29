d8f35e5bd30838297e8d6677bd7f1e1a1e14329b
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2025-11-28 21:50:02 -0500

    email, boxes program

--- README.old.md
+++ README.old.md
@@ -1,4 +1,4 @@
 # Market
 
 Email: admin@example.com
-Password: AdminPassword123!
+Password:AdminPassword123!
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -7,17 +7,18 @@ const ProductCard = ({
   isInCart,
   subtotal,
   cartFull,
+  selected,
 }) => {
   const productId = item.id;
   const numericSubtotal = Number(subtotal) || 0;
 
   return (
-    <div className="product-card">
+    <div className={`product-card${selected ? " product-card--selected" : ""}`}>
       <img className="product-card__img" src={item.imageUrl} alt={item.name} />
       <h4 className="product-card__title">{item?.name}</h4>
       <p className="product-card__origin">{item?.origin}</p>
       <div>{item?.quantity}</div>
-      {!isInCart && (
+      {item.category_id === 6 && !isInCart && (
         <button
           className="product-card__button"
           onClick={() => addToCart(item.id)}
